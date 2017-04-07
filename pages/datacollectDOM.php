8fd80e2c90c3d46d0e92e83a67a0cb3f3434ab42
user_name <email_id>
2017-04-07 10:23:33 +0200

    Wernli

--- pages/datacollectDOM.php
+++ pages/datacollectDOM.php
@@ -1,4 +1,11 @@
 <?php
 $url = "http://zuerich.usgang.ch/events.php";
 
+	$htmlContent = file_get_contents("http://zuerich.usgang.ch/events.php");
+
+	$DOM = new DOMDocument();
+	$DOM->loadHTML($htmlContent);
+
+	$Detail = $DOM.getElementsByClassName("inhalthell");
+    print($Detail);
 ?>

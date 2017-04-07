0866fc18e95f6c1a1de6008d10aae3f25e95565c
aladinbouddat <aladin.bouddat@gmail.com>
2017-04-07 10:32:20 +0200

    aladin

--- pages/datacollectDOM.php
+++ pages/datacollectDOM.php
@@ -5,5 +5,5 @@ $htmlContent = file_get_contents("http://zuerich.usgang.ch/events.php");
 $DOM = new DOMDocument();
 $DOM->loadHTML($htmlContent);
 
-$content = $DOM->getElementsByTagName('tbody');
+$content = $DOM->getElementsByClassName('inhalthell');
 ?>

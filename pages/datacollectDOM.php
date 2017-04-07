337f025abe94c7f7a9f4a28a006f6246d36a362e
aladinbouddat <aladin.bouddat@gmail.com>
2017-04-07 10:26:09 +0200

    Merge branch 'master' of /opt/lampp/htdocs/phpkurs/amproject/amproject with conflicts.

--- pages/datacollectDOM.php
+++ pages/datacollectDOM.php
@@ -1,11 +1,9 @@
 <?php
-$url = "http://zuerich.usgang.ch/events.php";
 
-	$htmlContent = file_get_contents("http://zuerich.usgang.ch/events.php");
+$htmlContent = file_get_contents("http://zuerich.usgang.ch/events.php");
 
-	$DOM = new DOMDocument();
-	$DOM->loadHTML($htmlContent);
+$DOM = new DOMDocument();
+$DOM->loadHTML($htmlContent);
 
-	$Detail = $DOM.getElementsByClassName("inhalthell");
-    print($Detail);
+$content = $DOM->getElementsByTagName('tbody');
 ?>

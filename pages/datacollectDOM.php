454c2f350b1a318b440a69d78c0ae9031b86f737
aladinbouddat <aladin.bouddat@gmail.com>
2017-05-09 11:10:37 +0200

    delete

--- pages/datacollectDOM.php
+++ pages/datacollectDOM.php
@@ -1,19 +1,31 @@
 <?php
 
-$htmlContent = file_get_contents("http://bern.usgang.ch/events.php");
+$htmlContent = file_get_contents("http://zuerich.usgang.ch/events.php");
 
 $DOM = new DOMDocument();
 $DOM->loadHTML($htmlContent);
 
 $content = $DOM->getElementsByTagName('td');
 
-$header =  array('Party','Location','Musikstil','Acts','Zeit');
 
 foreach($content as $NodeHeader)
 {
-    $aDataTableDetailHTML[] = trim($NodeHeader->textContent);
+    $Data[] = trim($NodeHeader->textContent);
 }
-print_r($aDataTableDetailHTML);
+
+
+$delete[] = [0,1,2,3,4,5,6,7,8,9,20];
+
+foreach ($delete as $d)
+{
+    unset($Data[$d]);
+}
+
+//unset($Data[0], $Data[1], $Data[2], $Data[3], $Data[4], $Data[5], $Data[6], $Data[7], $Data[8], $Data[9], $Data[20], $Data[31], $Data[32], $Data[33], $Data[34], $Data[35], $Data[36], $Data[47], $Data[56], $Data[136], $Data[236], $Data[276], $Data[321] );
+
+print_r($Data);
+
+$header =  array('Party','Location','Musikstil','Acts','Zeit');
 
 foreach ($header as $tableheader)
 {

f65c250d6175db2101dfc4c8e20349d251017ebd
user_name <email_id>
2017-05-10 17:24:58 +0200

    Wernli 10-Mai

--- pages/datacollectDOM.php
+++ pages/datacollectDOM.php
@@ -1,7 +1,6 @@
 <?php
 
 $htmlContent = file_get_contents("http://zuerich.usgang.ch/events.php");
-
 $DOM = new DOMDocument();
 $DOM->loadHTML($htmlContent);
 
@@ -11,34 +10,18 @@ $j=0;
 $c=0;
 
 foreach($content as $NodeHeader)
-{
-    $j+= 1;
-    $Data[] = trim($NodeHeader->textContent);
-}
-
-
-
-
-
+{   $j+= 1;
+    $Data[] = trim($NodeHeader->textContent);}
 $Delete = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 20);
 
 foreach ($Delete as $D)
-{
-    unset($Data[$D]);
-}
-
+{    unset($Data[$D]);}
 
 $key = array_search("© cinergy in Partnerschaft mit usgang.ch, Kalendereinträge/-promotion über eventbooster.ch", $Data);
 
 foreach ($Data as $D)
-{
-    unset($Data[$key+$c]);
-    $c+=1;
-}
+{    unset($Data[$key+$c]);
+    $c+=1;}
 
 $FinalData = array_values($Data);
-
-print_r($FinalData);
-
-
 ?>

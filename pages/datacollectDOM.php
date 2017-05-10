388ead2ac9ee04140ab36270ae64f06804b180a2
aladinbouddat <aladin.bouddat@gmail.com>
2017-05-10 17:23:40 +0200

    final array complete V2

--- pages/datacollectDOM.php
+++ pages/datacollectDOM.php
@@ -8,12 +8,16 @@ $DOM->loadHTML($htmlContent);
 $content = $DOM->getElementsByTagName('td');
 
 $j=0;
+$c=0;
+
 foreach($content as $NodeHeader)
 {
     $j+= 1;
     $Data[] = trim($NodeHeader->textContent);
-    if ($j==106){break;}
 }
+
+
+
 
 
 $Delete = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 20);
@@ -21,6 +25,15 @@ $Delete = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 20);
 foreach ($Delete as $D)
 {
     unset($Data[$D]);
+}
+
+
+$key = array_search("© cinergy in Partnerschaft mit usgang.ch, Kalendereinträge/-promotion über eventbooster.ch", $Data);
+
+foreach ($Data as $D)
+{
+    unset($Data[$key+$c]);
+    $c+=1;
 }
 
 $FinalData = array_values($Data);

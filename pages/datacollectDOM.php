5eee98499196262e25a10cd8ed2b27502d6bf7ed
aladinbouddat <aladin.bouddat@gmail.com>
2017-05-10 16:10:27 +0200

    final array complete

--- pages/datacollectDOM.php
+++ pages/datacollectDOM.php
@@ -7,38 +7,25 @@ $DOM->loadHTML($htmlContent);
 
 $content = $DOM->getElementsByTagName('td');
 
-
+$j=0;
 foreach($content as $NodeHeader)
 {
+    $j+= 1;
     $Data[] = trim($NodeHeader->textContent);
+    if ($j==106){break;}
 }
 
 
-$delete[] = [0,1,2,3,4,5,6,7,8,9,20];
+$Delete = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 20);
 
-foreach ($delete as $d)
+foreach ($Delete as $D)
 {
-    unset($Data[$d]);
+    unset($Data[$D]);
 }
 
-//unset($Data[0], $Data[1], $Data[2], $Data[3], $Data[4], $Data[5], $Data[6], $Data[7], $Data[8], $Data[9], $Data[20], $Data[31], $Data[32], $Data[33], $Data[34], $Data[35], $Data[36], $Data[47], $Data[56], $Data[136], $Data[236], $Data[276], $Data[321] );
+$FinalData = array_values($Data);
 
-print_r($Data);
+print_r($FinalData);
 
-$header =  array('Party','Location','Musikstil','Acts','Zeit');
 
-foreach ($header as $tableheader)
-{
-    $aDataTableHeaderHTML[] = trim($tableheader->textContent);
-}
-
-$i = 0;
-$j = 0;
-foreach($content as $tablecontent)
-{
-    $aDataTableContentHTML[$j][] = trim($tablecontent->textContent);
-    $i = $i + 1;
-    $j = $i % count($aDataTableHeaderHTML) == 0 ? $j + 1 : $j;
-}
-print_r($aDataTableContentHTML); die();
 ?>
